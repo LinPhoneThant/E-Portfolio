045c831db249f125d1d4d4de89d57943164831f6
LinPhoneThant <linphonethant7960@gmail.com>
2024-08-08 20:36:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,3 +19,28 @@ document.querySelector('.scroll-to-top').addEventListener('click', function(e) {
         behavior: 'smooth'
     });
 });
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 2000); 
